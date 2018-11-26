5bc3a1f47d60c9bbad9a296a63d069ffdd1c14e4
Ayupov Ruslan <syne10@yandex.ru>
2018-11-26 11:35:39 +0500

    rewrite for ES6

--- apple&bread/js/main.js
+++ apple&bread/js/main.js
@@ -1,13 +1,12 @@
-let bank  = prompt("сколько у вас денег?");
-let apple = prompt("сколько яблок вы собираетесь купить?");
-let bread = prompt("сколько хлеба вы собираетесь купить?");
+let budget     = prompt("сколько у вас денег?");
+let apple 	   = prompt("сколько яблок вы собираетесь купить?");
+let bread      = prompt("сколько хлеба вы собираетесь купить?");
 let applePrice = prompt("цена за одно яблоко?");
 let breadPrice = prompt("цена за одну булку хлеба?");
 
-let sum = (apple*applePrice) + (bread*breadPrice);
-
-if(bank>sum){
-  alert('true');
-} else {
-  alert('false');
+let calc = (budget, apple, bread, applePrice, breadPrice) => {
+	let sum = (apple*applePrice) + (bread*breadPrice);
+	return (budget > sum) ? console.log("Вам хватает денег на покупку!") : console.log("Вам не хватит денег!");
 }
+
+(budget != "" && apple != "" && bread != "" && applePrice != "" && breadPrice != "") ? calc() : console.log("Вы что-то упустили!");
